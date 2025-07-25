876c4a56ca84c0dc97d55ae75b8b4c59caf0f88e
Krishna Acharya <109304993+krishna-ji@users.noreply.github.com>
2025-07-25 23:33:58 +0545

    @krishna-ji: Before Advanced Population Initialization lgoic

--- config/ga_params.py
+++ config/ga_params.py
@@ -3,3 +3,4 @@
 POP_SIZE = 50
 NGEN = 100
 CXPB, MUTPB = 0.8, 0.3  # Increased crossover, reduced mutation for better convergence
+#  POP: size 200, ngen = 1500; CXPB, MUTPB =  0.8, 0.3 ma ramro result ako thyo.
